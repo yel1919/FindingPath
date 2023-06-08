9c61f6f074d933fad24eb8ce0ffc0b16cc596b3f
yel1919 <eliseev-kolya-yel@yandex.ru>
2023-06-08 22:24:11 +0500

    Add settings

--- FindResult/findresult.cpp
+++ FindResult/findresult.cpp
@@ -5,3 +5,37 @@ find_path::FindResult::FindResult()
       path(nullptr),
       success(false)
 {}
+
+find_path::FindResult::FindResult(const FindResult& result)
+    :
+      path(result.path),
+      success(result.success)
+{}
+
+find_path::FindResult::FindResult(FindResult&& result)
+    :
+      path(result.path),
+      success(result.success)
+{
+    result.path = nullptr;
+    result.success = false;
+}
+
+find_path::FindResult& find_path::FindResult::operator=(const FindResult& result) {
+    if(&result != this) {
+        path = result.path;
+        success = result.success;
+    }
+    return *this;
+}
+
+find_path::FindResult& find_path::FindResult::operator=(FindResult&& result) {
+    if(&result != this) {
+        path = result.path;
+        success = result.success;
+
+        result.path = nullptr;
+        result.success = false;
+    }
+    return *this;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDir>
+#include <QSettings>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -10,15 +12,16 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+private:
+    Ui::MainWindow *ui;
+    QSettings* settings;
 
+    void Save();
+    void Load();
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
 private slots:
     void on_pbGenerate_clicked();
-
-private:
-    Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
